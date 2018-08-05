9871ade4cb2287ecb600182e3b831598fd42c212
Monica  Karasek <mlkarasek@gmail.com>
2018-08-05 23:14:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,22 +12,21 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE if NOT EXISTS dogs(id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+    CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
     SQL
+    DB[:conn].execute(sql)
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
 
-  def self.drop_table
-  sql = "DROP TABLE IF EXISTS dogs"
-  DB[:conn].execute(sql)
-  end
-
   def self.new_from_db(row)
-  id = row[0]
-  name = row [1]
-  breed = row [2]
-  self.new(id: id, name: name, breed: breed)
-  end
+   id = row[0]
+   name = row[1]
+   breed = row[2]
+   self.new(id: id, name: name, breed: breed)
+ end
 
   def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
@@ -39,7 +38,7 @@ class Dog
       end
     dog
   end
-  
+
     def self.find_by_name(name)
       sql = <<-SQL
         SELECT * FROM dogs WHERE name = ? LIMIT 1
